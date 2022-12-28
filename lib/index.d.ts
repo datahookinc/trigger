abce8112eb4f1d1b91b4ca790fbedb984ad5d9d3
josh-weston <joshua.scott.weston@gmail.com>
2022-12-28 13:06:44 -0400

    0.7.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,16 +1,19 @@
 /** Autoincrementing primary key required for tables */
-declare type PK = number;
-declare type TableNotify = 'rowInsert' | 'rowDelete' | 'rowUpdate';
-declare type RowNotify = 'rowUpdate' | 'rowDelete';
-declare type AllowedPrimitives = string | number | Date | boolean | null;
-declare type TableEntry = {
+type PK = number;
+type TableNotify = 'rowInsert' | 'rowDelete' | 'rowUpdate';
+type RowNotify = 'rowUpdate' | 'rowDelete';
+type AllowedPrimitives = string | number | Date | boolean | null;
+type UserEntry = {
     [index: string]: AllowedPrimitives;
+};
+type TableEntry<T> = {
+    [K in keyof T]: T[K];
 } & {
-    _pk: PK;
+    _pk: number;
 };
 export interface Store {
     tables?: {
-        [index: string]: Table<ReturnType<(<T extends TableEntry>() => T)>>;
+        [index: string]: Table<ReturnType<(<T extends UserEntry>() => T)>>;
     };
     queues?: {
         [index: string]: Queue<unknown>;
@@ -19,34 +22,34 @@ export interface Store {
         [index: string]: Single<unknown>;
     };
 }
-export declare type DefinedTable<T> = {
+export type DefinedTable<T> = {
     [K in keyof T]: T[K][];
 };
-export declare type Table<T extends TableEntry> = {
-    use(where: ((v: T) => boolean) | null, notify?: TableNotify[]): T[];
-    useRow(pk: PK, notify?: RowNotify[]): T | undefined;
-    insertRow(r: Omit<T, '_pk'>): T | undefined;
-    insertRows(r: Omit<T, '_pk'>[], batchNotify?: boolean): T[];
-    onBeforeInsert(fn: (v: T) => T | void | boolean): void;
-    onAfterInsert(fn: (v: T) => void): void;
-    deleteRow(where: PK | Partial<Omit<T, '_pk'>> | ((v: T) => boolean)): boolean;
-    deleteRows(where?: Partial<Omit<T, '_pk'>> | ((v: T) => boolean), batchNotify?: boolean): number;
-    onBeforeDelete(fn: (v: T) => boolean | void): void;
-    onAfterDelete(fn: (v: T) => void): void;
-    updateRow(pk: PK, newValue: Partial<Omit<T, '_pk'>> | ((v: T) => Partial<Omit<T, '_pk'>>)): T | undefined;
-    updateRows(setValue: Partial<Omit<T, '_pk'>> | ((v: T) => Partial<Omit<T, '_pk'>>), where?: Partial<Omit<T, '_pk'>> | ((v: T) => boolean), batchNotify?: boolean): T[];
-    onBeforeUpdate(fn: (currentValue: T, newValue: T) => T | void | boolean): void;
-    onAfterUpdate(fn: (previousValue: T, newValue: T) => void): void;
-    getRows(where?: Partial<Omit<T, '_pk'>> | ((v: T) => boolean)): T[];
-    getRow(where: PK | Partial<Omit<T, '_pk'>> | ((v: T) => boolean)): T | undefined;
-    getRowCount(where?: Partial<Omit<T, '_pk'>> | ((v: T) => boolean)): number;
+export type Table<T extends UserEntry> = {
+    use(where: ((v: TableEntry<T>) => boolean) | null, notify?: TableNotify[]): TableEntry<T>[];
+    useRow(pk: PK, notify?: RowNotify[]): TableEntry<T> | undefined;
+    insertRow(r: T): TableEntry<T> | undefined;
+    insertRows(r: T[], batchNotify?: boolean): TableEntry<T>[];
+    onBeforeInsert(fn: (v: TableEntry<T>) => TableEntry<T> | void | boolean): void;
+    onAfterInsert(fn: (v: TableEntry<T>) => void): void;
+    deleteRow(where: PK | Partial<T> | ((v: TableEntry<T>) => boolean)): boolean;
+    deleteRows(where?: Partial<T> | ((v: TableEntry<T>) => boolean), batchNotify?: boolean): number;
+    onBeforeDelete(fn: (v: TableEntry<T>) => boolean | void): void;
+    onAfterDelete(fn: (v: TableEntry<T>) => void): void;
+    updateRow(pk: PK, newValue: Partial<T> | ((v: TableEntry<T>) => Partial<T>)): TableEntry<T> | undefined;
+    updateRows(setValue: Partial<T> | ((v: TableEntry<T>) => Partial<T>), where?: Partial<T> | ((v: TableEntry<T>) => boolean), batchNotify?: boolean): TableEntry<T>[];
+    onBeforeUpdate(fn: (currentValue: TableEntry<T>, newValue: TableEntry<T>) => TableEntry<T> | void | boolean): void;
+    onAfterUpdate(fn: (previousValue: TableEntry<T>, newValue: TableEntry<T>) => void): void;
+    getRows(where?: Partial<T> | ((v: TableEntry<T>) => boolean)): TableEntry<T>[];
+    getRow(where: PK | Partial<T> | ((v: TableEntry<T>) => boolean)): TableEntry<T> | undefined;
+    getRowCount(where?: Partial<T> | ((v: TableEntry<T>) => boolean)): number;
 };
-export declare function CreateTable<T extends TableEntry>(t: DefinedTable<T>): Table<T>;
-export declare type QueueItem<T> = {
+export declare function CreateTable<T extends UserEntry>(t: DefinedTable<T>): Table<TableEntry<T>>;
+export type QueueItem<T> = {
     item: T;
     cb?: (ok: boolean) => void;
 };
-export declare type Queue<T> = {
+export type Queue<T> = {
     insert(item: T, cb?: (ok: boolean) => void): boolean;
     onInsert(fn: (v: T) => void): void;
     get(): QueueItem<T> | undefined;
@@ -58,7 +61,7 @@ export declare type Queue<T> = {
  * @returns TriggerQueue<T>
  */
 export declare function CreateQueue<T>(): Queue<T>;
-export declare type Single<T> = {
+export type Single<T> = {
     use(): T;
     set(v: T): boolean;
     onSet(fn: (v: T) => void): void;
@@ -66,19 +69,19 @@ export declare type Single<T> = {
     get(): T;
 };
 export declare function CreateSingle<T>(s: T): Single<T>;
-declare type ExtractTables<T> = {
+type ExtractTables<T> = {
     readonly [K in keyof Omit<T, 'onInsert'>]: T[K] extends Record<PropertyKey, unknown> ? ExtractTables<T[K]> : T[K];
 };
 export declare function extractTables<T extends Store['tables']>(t: T): ExtractTables<T>;
-declare type ExtractQueues<T> = {
+type ExtractQueues<T> = {
     readonly [K in keyof Omit<T, 'onInsert' | 'onGet'>]: T[K] extends Record<PropertyKey, unknown> ? ExtractQueues<T[K]> : T[K];
 };
 export declare function extractQueues<T extends Store['queues']>(t: T): ExtractQueues<T>;
-declare type ExtractSingles<T> = {
+type ExtractSingles<T> = {
     readonly [K in keyof Omit<T, 'onSet' | 'onGet'>]: T[K] extends Record<PropertyKey, unknown> ? ExtractSingles<T[K]> : T[K];
 };
 export declare function extractSingles<T extends Store['singles']>(t: T): ExtractSingles<T>;
-declare type Extracted<T extends Store> = {
+type Extracted<T extends Store> = {
     tables: ExtractTables<T['tables']>;
     singles: ExtractSingles<T['singles']>;
     queues: ExtractQueues<T['queues']>;
